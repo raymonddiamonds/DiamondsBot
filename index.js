6ff99f0568635c89a3c321cfaaa64a82dee7ca54
Raymond Diamonds <raymond.diamonds@yahoo.ca>
2018-01-26 20:03:08 -0500

    update

--- index.js
+++ index.js
@@ -1,154 +1,200 @@
-'use strict'
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
 
-const app = express()
 
-app.set('port', (process.env.PORT || 5000))
 
-// Allows us to process the data
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
 
-// ROUTES
+// Server index page
+app.get("/", function (req, res) {
+    res.send("Deployed!");
+});
 
-app.get('/', function(req, res) {
-	res.send("Hi I am DiamondsBot")
-})
-
-let token = "EAAQRNZBHHfokBAFvGibreVIyZA1vQl279t01F4IsIAZBTDTSiPrZCt11p2Vc9jFrVyVYJpcLrx2EsIViBhMQ4zFjPnuCQFtQJiJ6Wvlq28w3QVOmzHiUKMuziBugOgzFGsBfwx7YiPVBg6cZBEeWZAHwe1UDQjcnyG6FiaPfAl3wZDZD"
-
-// Facebook
-
-app.get('/webhook/', function(req, res) {
-	if (req.query['hub.verify_token'] === "blondiebytes") {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send("Wrong token")
-})
-
-app.post('/webhook/', function(req, res) {
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = messaging_events[i]
-		let sender = event.sender.id
-		if (event.message && event.message.text) {
-			let text = event.message.text
-
-			decideMessage(sender, text)
-		}
-		if (event.postback) {
-			let text = JSON.stringify(event.postback)
-			decideMessage(sender, text)
-			continue
-		}
-	}
-	res.sendStatus(200)
-})
-function decideMessage(sender, text1) {
-	let text = text1.toLowerCase()
-	if (text.includes("summer")) {
-		sendImageMessage(sender)
-	} else if (text.includes("winter")) {
-			sendGenericMessage(sender)
-	} else {
-		sendText(sender, "I like Fall")
-		sendButtonMessage(sender, "What is your favourite season?")
-	}
-}
-
-function sendText(sender, text) {
-	let messageData = {text: text}
-	sendRequest(sender, messageData)
-}
-
-function sendButtonMessage(sender, text) {
-	let messageData = {
-		"message":{
-	"attachment":{
-		"type":"template",
-		"payload":{
-			"template_type":"button",
-			"text": text,
-			"buttons":[
-				{
-					"type":"postback",
-					"title":"Summer",
-					"payload":"summer"
-				},
-				{
-					"type":"postback",
-					"title":"Winter",
-					"payload":"winter"
-
-				}
-				},
-			]
-		}
-	}
-	sendRequest(sender, messageData)
-}
-
-function sendImageMessage(sender, text) {
-	let messageData = {
-    "attachment":{
-      "type":"image",
-      "payload":{
-        "url":"http://mefeater.com/wp-content/uploads/2015/06/summer.jpg",
-        "is_reusable":true
-      }
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
     }
-	}
-	sendRequest(sender, messageData)
-}
+});
 
-function sendGenericMessage(sender) {
-	let messageData = {"attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Winter!",
-						"imageURL": "http://images6.fanpop.com/image/photos/36200000/snow-image-snow-36241624-1600-1200.png",
-						"subtitle":"I love Winter!",
-						"buttons":[
-              {
-                "type":"web_url",
-                "url":"https://en.wikipedia.org/wiki/Winter",
-                "title":"More about Winter"
-              },
-            ]
-          }
-        ]
-      }
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function(entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
     }
-	}
-	}
-	sendRequest(sender, messageData)
+});
+
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
+
+    if (payload === "Greeting") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function(error, response, body) {
+            var greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " +  error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+            sendMessage(senderId, {text: message});
+        });
+    } else if (payload === "Correct") {
+        sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+    } else if (payload === "Incorrect") {
+        sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
+    }
 }
 
-//request to Facebook
-function sendRequest(sender, messageData){
-	request({
-		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token: token},
-		method: "POST",
-		json: {
-			recipient: {id: sender},
-			message : messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log("sending error")
-		} else if (response.body.error) {
-			console.log("response body error")
-		}
-	})
+function processMessage(event) {
+    if (!event.message.is_echo) {
+        var message = event.message;
+        var senderId = event.sender.id;
+
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
+
+        // You may get a text or attachment but not both
+        if (message.text) {
+            var formattedMsg = message.text.toLowerCase().trim();
+
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding movie detail.
+            // Otherwise search for new movie.
+            switch (formattedMsg) {
+                case "plot":
+                case "date":
+                case "runtime":
+                case "director":
+                case "cast":
+                case "rating":
+                    getMovieDetail(senderId, formattedMsg);
+                    break;
+
+                default:
+                    findMovie(senderId, formattedMsg);
+            }
+        } else if (message.attachments) {
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+        }
+    }
 }
 
-app.listen(app.get('port'), function() {
-	console.log("running: port")
-})
+function findMovie(userId, movieTitle) {
+    request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movieObj = JSON.parse(body);
+            if (movieObj.Response === "True") {
+                var query = {user_id: userId};
+                var update = {
+                    user_id: userId,
+                    title: movieObj.Title,
+                    plot: movieObj.Plot,
+                    date: movieObj.Released,
+                    runtime: movieObj.Runtime,
+                    director: movieObj.Director,
+                    cast: movieObj.Actors,
+                    rating: movieObj.imdbRating,
+                    poster_url:movieObj.Poster
+                };
+                var options = {upsert: true};
+                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+                    if (err) {
+                        console.log("Database error: " + err);
+                    } else {
+                        message = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "generic",
+                                    elements: [{
+                                        title: movieObj.Title,
+                                        subtitle: "Is this the movie you are looking for?",
+                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                                        buttons: [{
+                                            type: "postback",
+                                            title: "Yes",
+                                            payload: "Correct"
+                                        }, {
+                                            type: "postback",
+                                            title: "No",
+                                            payload: "Incorrect"
+                                        }]
+                                    }]
+                                }
+                            }
+                        };
+                        sendMessage(userId, message);
+                    }
+                });
+            } else {
+                console.log(movieObj.Error);
+                sendMessage(userId, {text: movieObj.Error});
+            }
+        } else {
+            sendMessage(userId, {text: "Something went wrong. Try again."});
+        }
+    });
+}
+
+function getMovieDetail(userId, field) {
+    Movie.findOne({user_id: userId}, function(err, movie) {
+        if(err) {
+            sendMessage(userId, {text: "Something went wrong. Try again"});
+        } else {
+            sendMessage(userId, {text: movie[field]});
+        }
+    });
+}
+
+// sends message to user
+function sendMessage(recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
+}
