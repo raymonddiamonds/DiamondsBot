4607ee7fa876851050644e8023db5aec29daaa7c
Raymond Diamonds <raymond.diamonds@yahoo.ca>
2018-02-13 17:36:04 -0500

    og

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ var db = mongoose.connect(process.env.MONGODB_URI);
 var Movie = require("./models/movie");
 
 var app = express();
-
-const movies = require('./movies.js')
-const request = require('request');
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
@@ -22,200 +18,184 @@ app.get("/", function (req, res) {
 
 // Facebook Webhook
 // Used for verification
-/* For Facebook Validation */
-app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === process.env.VERIFICATION_TOKEN) {
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    res.status(403).end();
-  }
+app.get("/webhook", function (req, res) {
+    if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+        console.log("Verified webhook");
+        res.status(200).send(req.query["hub.challenge"]);
+    } else {
+        console.error("Verification failed. The tokens do not match.");
+        res.sendStatus(403);
+    }
 });
 
-/* Handling all messenges */
-app.post("/webhook", (req, res) => {
-  console.log(req.body);
-  if (req.body.object === "page") {
-    req.body.entry.forEach((entry) => {
-      entry.messaging.forEach((event) => {
-        if (event.message && event.message.text) {
-          sendMessage(event);
-        }
-      });
-    });
-    res.status(200).end();
-  }
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+    // Make sure this is a page subscription
+    if (req.body.object == "page") {
+        // Iterate over each entry
+        // There may be multiple entries if batched
+        req.body.entry.forEach(function(entry) {
+            // Iterate over each messaging event
+            entry.messaging.forEach(function(event) {
+                if (event.postback) {
+                    processPostback(event);
+                } else if (event.message) {
+                    processMessage(event);
+                }
+            });
+        });
+
+        res.sendStatus(200);
+    }
 });
 
-function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
+function processPostback(event) {
+    var senderId = event.sender.id;
+    var payload = event.postback.payload;
 
-  request({
-    url: "https://graph.facebook.com/v2.6/" + sender,
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: text}
+    if (payload === "Greeting") {
+        // Get user's first name from the User Profile API
+        // and include it in the greeting
+        request({
+            url: "https://graph.facebook.com/v2.6/" + senderId,
+            qs: {
+                access_token: process.env.PAGE_ACCESS_TOKEN,
+                fields: "first_name"
+            },
+            method: "GET"
+        }, function(error, response, body) {
+            var greeting = "";
+            if (error) {
+                console.log("Error getting user's name: " +  error);
+            } else {
+                var bodyObj = JSON.parse(body);
+                name = bodyObj.first_name;
+                greeting = "Hi " + name + ". ";
+            }
+            var message = greeting + "My name is DiamondsBot. I can tell you various details regarding movies. What movie would you like to know about?";
+            sendMessage(senderId, {text: message});
+        });
+    } else if (payload === "Correct") {
+        sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
+    } else if (payload === "Incorrect") {
+        sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
     }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
-    
-  });
-  sendMessage(sender, {text: "hello"});
 }
 
-// function processPostback(event) {
-//     var senderId = event.sender.id;
-//     var payload = event.postback.payload;
+function processMessage(event) {
+    if (!event.message.is_echo) {
+        var message = event.message;
+        var senderId = event.sender.id;
 
-//     if (payload === "Greeting") {
-//         // Get user's first name from the User Profile API
-//         // and include it in the greeting
-//         request({
-//             url: "https://graph.facebook.com/v2.6/" + senderId,
-//             qs: {
-//                 access_token: process.env.PAGE_ACCESS_TOKEN,
-//                 fields: "first_name"
-//             },
-//             method: "GET"
-//         }, function(error, response, body) {
-//             var greeting = "";
-//             if (error) {
-//                 console.log("Error getting user's name: " +  error);
-//             } else {
-//                 var bodyObj = JSON.parse(body);
-//                 name = bodyObj.first_name;
-//                 greeting = "Hi " + name + ". ";
-//             }
-//             var message = greeting + "My name is DiamondsBot. I can tell you various details regarding movies. What movie would you like to know about?";
-//             sendMessage(senderId, {text: message});
-//         });
-//     } else if (payload === "Correct") {
-//         sendMessage(senderId, {text: "Awesome! What would you like to find out? Enter 'plot', 'date', 'runtime', 'director', 'cast' or 'rating' for the various details."});
-//     } else if (payload === "Incorrect") {
-//         sendMessage(senderId, {text: "Oops! Sorry about that. Try using the exact title of the movie"});
-//     }
-// }
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
 
-// function processMessage(event) {
-//     if (!event.message.is_echo) {
-//         var message = event.message;
-//         var senderId = event.sender.id;
+        // You may get a text or attachment but not both
+        if (message.text) {
+            var formattedMsg = message.text.toLowerCase().trim();
 
-//         console.log("Received message from senderId: " + senderId);
-//         console.log("Message is: " + JSON.stringify(message));
+            // If we receive a text message, check to see if it matches any special
+            // keywords and send back the corresponding movie detail.
+            // Otherwise search for new movie.
+            switch (formattedMsg) {
+                case "plot":
+                case "date":
+                case "runtime":
+                case "director":
+                case "cast":
+                case "rating":
+                    getMovieDetail(senderId, formattedMsg);
+                    break;
 
-//         // You may get a text or attachment but not both
-//         if (message.text) {
-//             var formattedMsg = message.text.toLowerCase().trim();
+                default:
+                    findMovie(senderId, formattedMsg);
+            }
+        } else if (message.attachments) {
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+        }
+    }
+}
 
-//             // If we receive a text message, check to see if it matches any special
-//             // keywords and send back the corresponding movie detail.
-//             // Otherwise search for new movie.
-//             switch (formattedMsg) {
-//                 case "plot":
-//                 case "date":
-//                 case "runtime":
-//                 case "director":
-//                 case "cast":
-//                 case "rating":
-//                     getMovieDetail(senderId, formattedMsg);
-//                     break;
+function findMovie(userId, movieTitle) {
+    request("http://www.omdbapi.com/?apikey=61557bae&t=" + movieTitle, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movieObj = JSON.parse(body);
+            if (movieObj.Response === "True") {
+                var query = {user_id: userId};
+                var update = {
+                    user_id: userId,
+                    title: movieObj.Title,
+                    plot: movieObj.Plot,
+                    date: movieObj.Released,
+                    runtime: movieObj.Runtime,
+                    director: movieObj.Director,
+                    cast: movieObj.Actors,
+                    rating: movieObj.imdbRating,
+                    poster_url:movieObj.Poster
+                };
+                var options = {upsert: true};
+                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
+                    if (err) {
+                        console.log("Database error: " + err);
+                    } else {
+                        message = {
+                            attachment: {
+                                type: "template",
+                                payload: {
+                                    template_type: "generic",
+                                    elements: [{
+                                        title: movieObj.Title,
+                                        subtitle: "Is this the movie you are looking for?",
+                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
+                                        buttons: [{
+                                            type: "postback",
+                                            title: "Yes",
+                                            payload: "Correct"
+                                        }, {
+                                            type: "postback",
+                                            title: "No",
+                                            payload: "Incorrect"
+                                        }]
+                                    }]
+                                }
+                            }
+                        };
+                        sendMessage(userId, message);
+                    }
+                });
+            } else {
+                console.log(movieObj.Error);
+                sendMessage(userId, {text: movieObj.Error});
+            }
+        } else {
+            sendMessage(userId, {text: "Something went wrong. Try again."});
+        }
+    });
+}
 
-//                 default:
-//                     findMovie(senderId, formattedMsg);
-//             }
-//         } else if (message.attachments) {
-//             sendMessage(senderId, {text: "Sorry, I don't understand your request."});
-//         }
-//     }
-// }
-
-// function findMovie(userId, movieTitle) {
-//     request("http://www.omdbapi.com/?apikey=61557bae&t=" + movieTitle, function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             var movieObj = JSON.parse(body);
-//             if (movieObj.Response === "True") {
-//                 var query = {user_id: userId};
-//                 var update = {
-//                     user_id: userId,
-//                     title: movieObj.Title,
-//                     plot: movieObj.Plot,
-//                     date: movieObj.Released,
-//                     runtime: movieObj.Runtime,
-//                     director: movieObj.Director,
-//                     cast: movieObj.Actors,
-//                     rating: movieObj.imdbRating,
-//                     poster_url:movieObj.Poster
-//                 };
-//                 var options = {upsert: true};
-//                 Movie.findOneAndUpdate(query, update, options, function(err, mov) {
-//                     if (err) {
-//                         console.log("Database error: " + err);
-//                     } else {
-//                         message = {
-//                             attachment: {
-//                                 type: "template",
-//                                 payload: {
-//                                     template_type: "generic",
-//                                     elements: [{
-//                                         title: movieObj.Title,
-//                                         subtitle: "Is this the movie you are looking for?",
-//                                         image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
-//                                         buttons: [{
-//                                             type: "postback",
-//                                             title: "Yes",
-//                                             payload: "Correct"
-//                                         }, {
-//                                             type: "postback",
-//                                             title: "No",
-//                                             payload: "Incorrect"
-//                                         }]
-//                                     }]
-//                                 }
-//                             }
-//                         };
-//                         sendMessage(userId, message);
-//                     }
-//                 });
-//             } else {
-//                 console.log(movieObj.Error);
-//                 sendMessage(userId, {text: movieObj.Error});
-//             }
-//         } else {
-//             sendMessage(userId, {text: "Something went wrong. Try again."});
-//         }
-//     });
-// }
-
-// function getMovieDetail(userId, field) {
-//     Movie.findOne({user_id: userId}, function(err, movie) {
-//         if(err) {
-//             sendMessage(userId, {text: "Something went wrong. Try again"});
-//         } else {
-//             sendMessage(userId, {text: movie[field]});
-//         }
-//     });
-// }
+function getMovieDetail(userId, field) {
+    Movie.findOne({user_id: userId}, function(err, movie) {
+        if(err) {
+            sendMessage(userId, {text: "Something went wrong. Try again"});
+        } else {
+            sendMessage(userId, {text: movie[field]});
+        }
+    });
+}
 
 // sends message to user
-// function sendMessage(recipientId, message) {
-//     request({
-//         url: "https://graph.facebook.com/v2.6/me/messages",
-//         qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-//         method: "POST",
-//         json: {
-//             recipient: {id: recipientId},
-//             message: message,
-//         }
-//     }, function(error, response, body) {
-//         if (error) {
-//             console.log("Error sending message: " + response.error);
-//         }
-//     });
-// }
+function sendMessage(recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log("Error sending message: " + response.error);
+        }
+    });
+}
