f49eb12314eff36f675c2c4498379459312724cc
Raymond <raymond.diamonds@yahoo.ca>
2019-01-07 20:07:30 -0500

    postback

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post("/webhook", function (req, res) {
         req.body.entry.forEach(function(entry) {
             // Iterate over each messaging event
             entry.messaging.forEach(function(event) {
-                console.log("test", event)
+               
                 if (event.postback) {
                     processPostback(event);
                 } else if (event.message) {
@@ -52,6 +52,35 @@ app.post("/webhook", function (req, res) {
         res.sendStatus(200);
     }
 });
+
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
+
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
+  }
+}
 
 function processMessage(event) {
     if (!event.message.is_echo) {
