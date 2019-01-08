2fd98b61c8ec3b8c4317f7a0df2b954c8a5b2e48
Raymond <raymond.diamonds@yahoo.ca>
2019-01-07 20:35:41 -0500

    weather

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var weather = require("./weatherAPI.js");
 
 
 
@@ -99,6 +100,7 @@ function processMessage(event) {
                       })
                     break;
                 case "Find weather info":
+                    sendMessage(senderId, {text: "For which city?"});
 
 
                 default:
@@ -110,72 +112,6 @@ function processMessage(event) {
     }
 }
 
-function findMovie(userId, movieTitle) {
-    request("http://www.omdbapi.com/?type=movie&t=" + movieTitle, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var movieObj = JSON.parse(body);
-            if (movieObj.Response === "True") {
-                var query = {user_id: userId};
-                var update = {
-                    user_id: userId,
-                    title: movieObj.Title,
-                    plot: movieObj.Plot,
-                    date: movieObj.Released,
-                    runtime: movieObj.Runtime,
-                    director: movieObj.Director,
-                    cast: movieObj.Actors,
-                    rating: movieObj.imdbRating,
-                    poster_url:movieObj.Poster
-                };
-                var options = {upsert: true};
-                Movie.findOneAndUpdate(query, update, options, function(err, mov) {
-                    if (err) {
-                        console.log("Database error: " + err);
-                    } else {
-                        message = {
-                            attachment: {
-                                type: "template",
-                                payload: {
-                                    template_type: "generic",
-                                    elements: [{
-                                        title: movieObj.Title,
-                                        subtitle: "Is this the movie you are looking for?",
-                                        image_url: movieObj.Poster === "N/A" ? "http://placehold.it/350x150" : movieObj.Poster,
-                                        buttons: [{
-                                            type: "postback",
-                                            title: "Yes",
-                                            payload: "Correct"
-                                        }, {
-                                            type: "postback",
-                                            title: "No",
-                                            payload: "Incorrect"
-                                        }]
-                                    }]
-                                }
-                            }
-                        };
-                        sendMessage(userId, message);
-                    }
-                });
-            } else {
-                console.log(movieObj.Error);
-                sendMessage(userId, {text: movieObj.Error});
-            }
-        } else {
-            sendMessage(userId, {text: "Something went wrong. Try again. (findMovie); " + userId + ", " +  movieTitle });
-        }
-    });
-}
-
-function getMovieDetail(userId, field) {
-    Movie.findOne({user_id: userId}, function(err, movie) {
-        if(err) {
-            sendMessage(userId, {text: "Something went wrong. Try again (movie details)"});
-        } else {
-            sendMessage(userId, {text: movie[field]});
-        }
-    });
-}
 
 // sends message to user
 function sendMessage(recipientId, message) {
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,37 +1,33 @@
-// var http = require('http')
+var http = require('http')
 
-// const apiKey = process.env.
+// Connect to API URL api.openweathermap.org/data/2.5/weather?q={city name}
+module.exports = function accessWeather(city, callback) {
 
-// // Connect to API URL api.openweathermap.org/data/2.5/weather?q={city name}
-// module.exports = function accessWeather(city, callback) {
+	var options = {
+		host: "api.openweathermap.org",
+		path: "/data/2.5/weather?q=" + city + "&appid=" + process.env.WEATHER_API_ID + "&units=metric"
+		method: "GET"
+	}
 
-// 	var options = {
-// 		host: "api.openweathermap.org",
-// 		path: "/data/2.5/weather?q=" + city + "&appid=" + apiKey + "",
-// 		method: "GET"
-// 	}
+	var body = ""
 
-// 	var body = ""
+	var request = http.request(options, function(response) {
 
-// 	var request = http.request(options, function(response) {
+		response.on('data', function(chunk) {
+			body += chunk.toString('utf8')
+		})
+		response.on('end', function() {
 
-// 		response.on('data', function(chunk) {
-// 			body += chunk.toString('utf8')
-// 		})
-// 		response.on('end', function() {
-
-// 			var json = JSON.parse(body)
-// 			if (json['cod'] == 502) {
-// 				callback("nac", "nac")
-// 				return
-// 			} 
-// 			var temperature = parseInt(json["main"]["temp"] - 273)
-// 			var description = json["weather"][0]["description"]
-// 			callback(temperature, description)
+			var json = JSON.parse(body)
+			if (json['cod'] == 502) {
+				callback("nac", "nac")
+				return
+			} 
+			var temperature = parseInt(json["main"]["temp"])
+			var description = json["weather"][0]["description"]
+			callback(temperature, description)
 			
-// 		})
-// 	})
-// 	request.end()
-// }
-// 
-// efc2db82be8a9da5ad11bed26df76480
+		})
+	})
+	request.end()
+}
