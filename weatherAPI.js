b9fa97e0bfe8baac2c2c447d3cc713344ac7c9ab
Raymond <raymond.diamonds@yahoo.ca>
2019-01-07 20:41:05 -0500

    debug weather

--- weatherAPI.js
+++ weatherAPI.js
@@ -3,31 +3,44 @@ var http = require('http')
 // Connect to API URL api.openweathermap.org/data/2.5/weather?q={city name}
 module.exports = function accessWeather(city, callback) {
 
-	var options = {
-		host: "api.openweathermap.org",
-		path: "/data/2.5/weather?q=" + city + "&appid=" + process.env.WEATHER_API_ID + "&units=metric"
-		method: "GET"
-	}
+	// var options = {
+	// 	host: "api.openweathermap.org",
+	// 	path: "/data/2.5/weather?q=" + city + "&appid=" + process.env.WEATHER_API_ID + "&units=metric"
+	// 	method: "GET"
+	// }
 
-	var body = ""
+	// var body = ""
 
-	var request = http.request(options, function(response) {
+	// var request = http.request(options, function(response) {
 
-		response.on('data', function(chunk) {
-			body += chunk.toString('utf8')
-		})
-		response.on('end', function() {
+	// 	response.on('data', function(chunk) {
+	// 		body += chunk.toString('utf8')
+	// 	})
+	// 	response.on('end', function() {
 
-			var json = JSON.parse(body)
-			if (json['cod'] == 502) {
-				callback("nac", "nac")
-				return
-			} 
-			var temperature = parseInt(json["main"]["temp"])
-			var description = json["weather"][0]["description"]
-			callback(temperature, description)
+	// 		var json = JSON.parse(body)
+	// 		if (json['cod'] == 502) {
+	// 			callback("nac", "nac")
+	// 			return
+	// 		} 
+	// 		var temperature = parseInt(json["main"]["temp"])
+	// 		var description = json["weather"][0]["description"]
+	// 		callback(temperature, description)
 			
-		})
-	})
-	request.end()
+	// 	})
+	// })
+	// request.end()
+	// 
+	// 
+	// 
+	
+	url = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + process.env.WEATHER_API_ID + "&units=metric";
+
+	request(url, function (error, response, body) {
+
+		var json = JSON.parse(body);
+
+		callback(json.main.temp)
+    
+  	});
 }
