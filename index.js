751043e60005fc0367f02ea8d0cad20dd3cbb6a0
Raymond <raymond.diamonds@yahoo.ca>
2019-01-07 20:12:52 -0500

    test postback

--- index.js
+++ index.js
@@ -57,31 +57,8 @@ function processPostback(event) {
   var senderId = event.sender.id;
   var payload = event.postback.payload;
 
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
-      }
-      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
-      sendMessage(senderId, {text: message});
-    });
-  } else {
-    sendMessage(senderId, {text: "hello world postback!"});
-  }
+    sendMessage(senderId, {text: senderId});
+  
 }
 
 function processMessage(event) {
@@ -125,7 +102,7 @@ function processMessage(event) {
 
 
                 default:
-                    sendMessage(senderId, {text: message})
+                    sendMessage(senderId, {text: "Defaul message. Sorry I don't understand. Try again"})
             }
         } else if (message.attachments) {
             sendMessage(senderId, {text: "Sorry, I don't understand your request. Please try again"});
